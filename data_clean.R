26f75727d620c6509abc5a40ada0ae70e4164f9c
anastat <anastasia.teterina@gmail.com>
2023-09-18 11:46:56 -0400

    Initial commit

--- data_clean.R
+++ data_clean.R
@@ -5,7 +5,7 @@ getwd()
 raw.dat <- read.csv("raw_data/maternalmortality.csv", stringsAsFactors = FALSE)
 
 #select() function  subset the data to have only the variables Country.Name, X2000 – X2019
-dat <- raw.dat %>% select(Country.Name, X2000:X2019)
+dat.mat <- raw.dat %>% select(Country.Name, X2000:X2019)
 head(dat)
 
 # pivot_longer() function to convert the data set into a long format
@@ -13,9 +13,27 @@ head(dat)
 # remove the prefix X from them, 
 # change the name of the variable to Year, change the values to MatMor. 
 # Finally, make sure the year variable is stored as numeric
-dat.long <- dat %>% pivot_longer(X2000:X2019, names_prefix = "X", names_to = "Year", values_to = "MatMor", 
+dat.mat.long <- dat.mat %>% pivot_longer(X2000:X2019, names_prefix = "X", names_to = "Year", values_to = "MatMor", 
                                  names_transform = list(Year = as.double))
 # str(dat.long)
+
+# Download disaster.csv
+raw.dat <- read.csv("raw_data/disaster.csv", stringsAsFactors = FALSE)
+# Use the filter() function to subset the data set to only include years 2000–2019 and 
+# the disaster types “Earthquake” and “Drought”
+dat.dis <- raw.dat %>% filter(Year>1999 & Year <2020 & Disaster.Type %in% c("Earthquake", "Drought")) %>%
+# Subset the data set to only include the following variables: Year, ISO, Disaster.type.
+  select(Year, ISO, Disaster.Type) %>%
+# dummy variable drought and another dummy variable earthquake such that:
+  mutate(drought = ifelse(Disaster.Type == "Drought", 1, 0), 
+         earthquake = ifelse(Disaster.Type == "Earthquake", 1, 0)) %>%
+# Use the group_by() and summarize() functions to create a data set where only 
+  # one row of observation exists for each country and each year,
+  group_by(Year, ISO) %>% summarise(drought = ifelse(sum(drought) >0, 1, 0), 
+                                    earthquake = ifelse(sum(earthquake) >0, 1, 0))
+
+names(dat.dis)
+
 
 library(usethis)
 usethis::use_git_config(user.name = "anastat", user.email = "anastasia.teterina@gmail.com")
