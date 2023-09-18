b59de73dc39b681873996173625500a8930c820d
anastat <anastasia.teterina@gmail.com>
2023-09-18 11:25:15 -0400

    Initial commit

--- data_clean.R
+++ data_clean.R
@@ -14,6 +14,13 @@ head(dat)
 # change the name of the variable to Year, change the values to MatMor. 
 # Finally, make sure the year variable is stored as numeric
 dat.long <- dat %>% pivot_longer(X2000:X2019, names_prefix = "X", names_to = "Year", values_to = "MatMor", 
-                                 names_transform = list(Year = as.integer))
-str(dat.long)
+                                 names_transform = list(Year = as.double))
+# str(dat.long)
+
+library(usethis)
+usethis::use_git_config(user.name = "anastat", user.email = "anastasia.teterina@gmail.com")
+usethis::git_sitrep()
+
+usethis::use_git()
+
 
